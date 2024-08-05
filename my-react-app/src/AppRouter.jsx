8b0275e0c7423acc57ebab7c40971505ae0b799a
Subsidy2032 <noam133m@gmail.com>
2024-08-05 18:07:35 +0300

    first commit

---
+++ my-react-app/src/AppRouter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './Components/NavBar.jsx';
+import Home from './Components/Home.jsx';
+import Order from './Components/Order.jsx';
+import NotFound from './Components/NotFound.jsx';
+
+function AppRouter() {
+    return (
+        <Router>
+            <div>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/order" element={<Order/>} />
+                    <Route path="*" element={<NotFound/>} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default AppRouter;
---
+++ my-react-app/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router } from "react-router-dom";
+
+function Home() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Home;

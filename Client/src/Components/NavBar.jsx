a78e2edb7aff667526c54aa32f43c0b7cdafb0c1
Subsidy2032 <noam133m@gmail.com>
2024-08-15 16:57:31 +0300

    Completed NavBar

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css"
 
 function NavBar (props) {
     return (
-        <div>
+        <div className={styles.navbar}>
+            <nav>
+                <ul className={styles.ul}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/Order">Order</Link></li>
+                </ul>
+            </nav>
         </div>
     )
 }

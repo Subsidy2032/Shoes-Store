df19f518454e91fff1fb122aae61cbe58b34fb38
Subsidy2032 <noam133m@gmail.com>
2024-08-28 08:01:31 +0300

    Added logo

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import ShoppingCart from "./ShoppingCart";
+import logo from "../assets/logo.jpg"
 
 function NavBar() {
     const [cartVisible, setCartVisible] = useState(false);
@@ -13,6 +14,7 @@ function NavBar() {
     return (
         <div className={styles.navbar}>
             <nav>
+                <img src={logo} alt="Logo" className={styles.logo} />
                 <ul className={styles.ul}>
                     <li><Link to="/">Home</Link></li>
                     <li className={styles.cartContainer}>
